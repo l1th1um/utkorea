ba9b7f96e118e00b5cb4786d43c5a10b2a525d4a
l1th1um <4r53n1c@gmail.com>
2012-11-16 12:15:47 +0900

    finishing pendaftaran mahasiswa baru => generate pdf, send email with attachment

---
+++ portal/logs/log-2012-11-15.php
@@ -0,0 +1,107 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2012-11-15 07:19:17 --> Query error: Table 'utkorea.announcements' doesn't exist
+ERROR - 2012-11-15 07:21:12 --> Could not find the language line "profiler_section_hide"
+ERROR - 2012-11-15 07:21:12 --> Could not find the language line "profiler_section_show"
+ERROR - 2012-11-15 07:21:12 --> Could not find the language line "profiler_section_hide"
+ERROR - 2012-11-15 07:21:12 --> Could not find the language line "profiler_section_hide"
+ERROR - 2012-11-15 07:21:12 --> Could not find the language line "profiler_headers"
+ERROR - 2012-11-15 07:21:12 --> Could not find the language line "profiler_section_show"
+ERROR - 2012-11-15 07:21:12 --> Could not find the language line "profiler_section_hide"
+ERROR - 2012-11-15 07:21:12 --> Could not find the language line "profiler_section_show"
+ERROR - 2012-11-15 07:21:12 --> Could not find the language line "profiler_section_show"
+ERROR - 2012-11-15 07:21:12 --> Could not find the language line "profiler_session_data"
+ERROR - 2012-11-15 07:21:12 --> Could not find the language line "profiler_section_show"
+ERROR - 2012-11-15 07:21:12 --> Could not find the language line "profiler_section_hide"
+ERROR - 2012-11-15 07:21:12 --> Could not find the language line "profiler_section_show"
+ERROR - 2012-11-15 07:21:12 --> Could not find the language line "profiler_section_show"
+ERROR - 2012-11-15 07:21:12 --> Could not find the language line "profiler_config"
+ERROR - 2012-11-15 07:21:12 --> Could not find the language line "profiler_section_show"
+ERROR - 2012-11-15 07:21:12 --> Could not find the language line "profiler_section_hide"
+ERROR - 2012-11-15 07:21:12 --> Could not find the language line "profiler_section_show"
+ERROR - 2012-11-15 07:21:12 --> Could not find the language line "profiler_section_show"
+ERROR - 2012-11-15 07:57:24 --> Query error: Unknown column 'username' in 'where clause'
+ERROR - 2012-11-15 07:57:34 --> Severity: Notice  --> Undefined index: password D:\xampp\htdocs\utkorea\portal\libraries\auth.php 62
+ERROR - 2012-11-15 07:57:34 --> Severity: Notice  --> Undefined index: username D:\xampp\htdocs\utkorea\portal\libraries\auth.php 65
+ERROR - 2012-11-15 07:57:34 --> Severity: Notice  --> Undefined index: username D:\xampp\htdocs\utkorea\portal\libraries\auth.php 68
+ERROR - 2012-11-15 07:58:04 --> Query error: Unknown column 'username' in 'where clause'
+ERROR - 2012-11-15 07:58:46 --> Severity: Notice  --> Trying to get property of non-object D:\xampp\htdocs\utkorea\portal\helpers\common_helper.php 390
+ERROR - 2012-11-15 07:58:46 --> Severity: Notice  --> Trying to get property of non-object D:\xampp\htdocs\utkorea\portal\helpers\common_helper.php 390
+ERROR - 2012-11-15 08:49:29 --> Query error: Table 'utkorea.education_list' doesn't exist
+ERROR - 2012-11-15 08:49:43 --> Query error: Table 'utkorea.education_list' doesn't exist
+ERROR - 2012-11-15 09:10:36 --> Severity: Warning  --> mail() [<a href='function.mail'>function.mail</a>]: Failed to connect to mailserver at &quot;localhost&quot; port 25, verify your &quot;SMTP&quot; and &quot;smtp_port&quot; setting in php.ini or use ini_set() D:\xampp\htdocs\utkorea\base\libraries\Email.php 1553
+ERROR - 2012-11-15 09:35:59 --> Could not find the language line "year_certificate"
+ERROR - 2012-11-15 09:39:07 --> 404 Page Not Found --> daftar_ulang
+ERROR - 2012-11-15 09:39:39 --> Could not find the language line "place_of_birth"
+ERROR - 2012-11-15 09:39:39 --> Could not find the language line "birth_date"
+ERROR - 2012-11-15 09:39:39 --> Could not find the language line "employment"
+ERROR - 2012-11-15 09:39:39 --> Could not find the language line "last_education_major"
+ERROR - 2012-11-15 09:39:39 --> Could not find the language line "teach"
+ERROR - 2012-11-15 09:39:39 --> Could not find the language line "teach_at"
+ERROR - 2012-11-15 09:39:39 --> Could not find the language line "teach_major"
+ERROR - 2012-11-15 09:39:39 --> Could not find the language line "ijasah_image"
+ERROR - 2012-11-15 09:39:39 --> Could not find the language line "passport_image"
+ERROR - 2012-11-15 09:39:39 --> Could not find the language line "photo_image"
+ERROR - 2012-11-15 09:40:11 --> Could not find the language line "place_of_birth"
+ERROR - 2012-11-15 09:40:11 --> Could not find the language line "birth_date"
+ERROR - 2012-11-15 09:40:11 --> Could not find the language line "employment"
+ERROR - 2012-11-15 09:40:11 --> Could not find the language line "last_education_major"
+ERROR - 2012-11-15 09:40:11 --> Could not find the language line "teach"
+ERROR - 2012-11-15 09:40:11 --> Could not find the language line "teach_at"
+ERROR - 2012-11-15 09:40:11 --> Could not find the language line "teach_major"
+ERROR - 2012-11-15 09:40:11 --> Could not find the language line "ijasah_image"
+ERROR - 2012-11-15 09:40:11 --> Could not find the language line "passport_image"
+ERROR - 2012-11-15 09:40:11 --> Could not find the language line "photo_image"
+ERROR - 2012-11-15 09:43:53 --> Could not find the language line "teach"
+ERROR - 2012-11-15 09:43:53 --> Could not find the language line "teach_at"
+ERROR - 2012-11-15 09:43:53 --> Could not find the language line "teach_major"
+ERROR - 2012-11-15 21:06:23 --> Severity: Warning  --> require_once(E:/utkorea/portal/helpers/dompdf/lib/php-font-lib/classes/font.cls.php) [<a href='function.require-once'>function.require-once</a>]: failed to open stream: No such file or directory E:\utkorea\portal\helpers\dompdf\include\font_metrics.cls.php 13
+ERROR - 2012-11-15 22:12:46 --> Severity: Notice  --> Undefined property: CI_DB_mysql_result::$result E:\utkorea\portal\models\person_model.php 273
+ERROR - 2012-11-15 22:13:43 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 110
+ERROR - 2012-11-15 22:14:02 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 111
+ERROR - 2012-11-15 22:14:13 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 111
+ERROR - 2012-11-15 22:14:58 --> Severity: Notice  --> Undefined index:  name E:\utkorea\portal\views\pendaftaran\form_pdf.php 110
+ERROR - 2012-11-15 22:15:13 --> Severity: Notice  --> Undefined index:  name E:\utkorea\portal\views\pendaftaran\form_pdf.php 110
+ERROR - 2012-11-15 22:15:27 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 110
+ERROR - 2012-11-15 22:29:42 --> Severity: Notice  --> Use of undefined constant month_array - assumed 'month_array' E:\utkorea\portal\helpers\common_helper.php 276
+ERROR - 2012-11-15 22:45:01 --> Could not find the language line "employment_status_list"
+ERROR - 2012-11-15 22:49:50 --> Could not find the language line "employment_status_list"
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 114
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 119
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 114
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 125
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 119
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 131
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 125
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 131
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 131
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 136
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 131
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 141
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 136
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 146
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 141
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 151
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 146
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 158
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 151
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Undefined index:   E:\utkorea\portal\views\pendaftaran\form_pdf.php 158
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 158
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 166
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Undefined index:   E:\utkorea\portal\views\pendaftaran\form_pdf.php 158
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Undefined index:   E:\utkorea\portal\views\pendaftaran\form_pdf.php 166
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 166
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 174
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Undefined index:   E:\utkorea\portal\views\pendaftaran\form_pdf.php 166
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Undefined index:   E:\utkorea\portal\views\pendaftaran\form_pdf.php 174
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 174
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 182
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Undefined index:   E:\utkorea\portal\views\pendaftaran\form_pdf.php 174
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Undefined index:   E:\utkorea\portal\views\pendaftaran\form_pdf.php 182
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 182
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 188
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Undefined index:   E:\utkorea\portal\views\pendaftaran\form_pdf.php 182
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 193
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 188
+ERROR - 2012-11-15 23:08:50 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 193
+ERROR - 2012-11-15 23:28:49 --> Severity: Notice  --> Undefined variable: mother_name E:\utkorea\portal\views\pendaftaran\form_pdf.php 238
---
+++ portal/logs/log-2012-11-16.php
@@ -0,0 +1,69 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+ERROR - 2012-11-16 09:27:51 --> Severity: Warning  --> mail() [<a href='function.mail'>function.mail</a>]: SMTP server response: 550 relaying denied E:\utkorea\base\libraries\Email.php 1553
+ERROR - 2012-11-16 09:31:25 --> Severity: Warning  --> mail() [<a href='function.mail'>function.mail</a>]: SMTP server response: 550 relaying denied E:\utkorea\base\libraries\Email.php 1553
+ERROR - 2012-11-16 09:39:21 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 119
+ERROR - 2012-11-16 09:39:21 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 124
+ERROR - 2012-11-16 09:39:21 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 130
+ERROR - 2012-11-16 09:39:21 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 137
+ERROR - 2012-11-16 09:39:21 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 137
+ERROR - 2012-11-16 09:39:21 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 142
+ERROR - 2012-11-16 09:39:21 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 147
+ERROR - 2012-11-16 09:39:21 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 152
+ERROR - 2012-11-16 09:39:21 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 157
+ERROR - 2012-11-16 09:39:21 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 164
+ERROR - 2012-11-16 09:39:21 --> Severity: Notice  --> Undefined index:   E:\utkorea\portal\views\pendaftaran\form_pdf.php 164
+ERROR - 2012-11-16 09:39:21 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 172
+ERROR - 2012-11-16 09:39:21 --> Severity: Notice  --> Undefined index:   E:\utkorea\portal\views\pendaftaran\form_pdf.php 172
+ERROR - 2012-11-16 09:39:21 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 180
+ERROR - 2012-11-16 09:39:21 --> Severity: Notice  --> Undefined index:   E:\utkorea\portal\views\pendaftaran\form_pdf.php 180
+ERROR - 2012-11-16 09:39:21 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 188
+ERROR - 2012-11-16 09:39:21 --> Severity: Notice  --> Undefined index:   E:\utkorea\portal\views\pendaftaran\form_pdf.php 188
+ERROR - 2012-11-16 09:39:21 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 194
+ERROR - 2012-11-16 09:39:21 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 199
+ERROR - 2012-11-16 09:39:21 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 206
+ERROR - 2012-11-16 09:39:21 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 241
+ERROR - 2012-11-16 09:39:21 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 248
+ERROR - 2012-11-16 09:39:21 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 255
+ERROR - 2012-11-16 09:39:21 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 262
+ERROR - 2012-11-16 09:42:16 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 119
+ERROR - 2012-11-16 09:42:16 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 124
+ERROR - 2012-11-16 09:42:16 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 130
+ERROR - 2012-11-16 09:42:16 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 137
+ERROR - 2012-11-16 09:42:16 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 137
+ERROR - 2012-11-16 09:42:16 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 142
+ERROR - 2012-11-16 09:42:16 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 147
+ERROR - 2012-11-16 09:42:16 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 152
+ERROR - 2012-11-16 09:42:16 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 157
+ERROR - 2012-11-16 09:42:16 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 164
+ERROR - 2012-11-16 09:42:16 --> Severity: Notice  --> Undefined index:   E:\utkorea\portal\views\pendaftaran\form_pdf.php 164
+ERROR - 2012-11-16 09:42:16 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 172
+ERROR - 2012-11-16 09:42:16 --> Severity: Notice  --> Undefined index:   E:\utkorea\portal\views\pendaftaran\form_pdf.php 172
+ERROR - 2012-11-16 09:42:16 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 180
+ERROR - 2012-11-16 09:42:16 --> Severity: Notice  --> Undefined index:   E:\utkorea\portal\views\pendaftaran\form_pdf.php 180
+ERROR - 2012-11-16 09:42:16 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 188
+ERROR - 2012-11-16 09:42:16 --> Severity: Notice  --> Undefined index:   E:\utkorea\portal\views\pendaftaran\form_pdf.php 188
+ERROR - 2012-11-16 09:42:16 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 194
+ERROR - 2012-11-16 09:42:16 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 199
+ERROR - 2012-11-16 09:42:16 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 206
+ERROR - 2012-11-16 09:42:16 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 241
+ERROR - 2012-11-16 09:42:16 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 248
+ERROR - 2012-11-16 09:42:16 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 255
+ERROR - 2012-11-16 09:42:16 --> Severity: Notice  --> Trying to get property of non-object E:\utkorea\portal\views\pendaftaran\form_pdf.php 262
+ERROR - 2012-11-16 10:18:29 --> Tipe file yang anda coba upload tidak di perbolehkan.
+ERROR - 2012-11-16 10:18:43 --> Severity: Warning  --> Missing argument 3 for pendaftaran::mail_new_registrant(), called in E:\utkorea\portal\controllers\pendaftaran.php on line 31 and defined E:\utkorea\portal\controllers\pendaftaran.php 145
+ERROR - 2012-11-16 10:18:46 --> Severity: Warning  --> mail() [<a href='function.mail'>function.mail</a>]: SMTP server response: 550 relaying denied E:\utkorea\base\libraries\Email.php 1553
+ERROR - 2012-11-16 10:18:46 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at E:\utkorea\base\core\Exceptions.php:185) E:\utkorea\base\libraries\Session.php 672
+ERROR - 2012-11-16 10:21:00 --> Severity: Warning  --> mail() [<a href='function.mail'>function.mail</a>]: SMTP server response: 550 relaying denied E:\utkorea\base\libraries\Email.php 1553
+ERROR - 2012-11-16 10:21:00 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at E:\utkorea\base\core\Exceptions.php:185) E:\utkorea\base\libraries\Session.php 672
+ERROR - 2012-11-16 10:24:04 --> Severity: Warning  --> mail() [<a href='function.mail'>function.mail</a>]: SMTP server response: 550 relaying denied E:\utkorea\base\libraries\Email.php 1553
+ERROR - 2012-11-16 10:24:04 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at E:\utkorea\base\core\Exceptions.php:185) E:\utkorea\base\libraries\Session.php 672
+ERROR - 2012-11-16 10:28:44 --> Severity: Warning  --> mail() [<a href='function.mail'>function.mail</a>]: SMTP server response: 550 relaying denied E:\utkorea\base\libraries\Email.php 1553
+ERROR - 2012-11-16 10:28:44 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at E:\utkorea\base\core\Exceptions.php:185) E:\utkorea\base\libraries\Session.php 672
+ERROR - 2012-11-16 10:31:02 --> Severity: Warning  --> mail() [<a href='function.mail'>function.mail</a>]: SMTP server response: 550 relaying denied E:\utkorea\base\libraries\Email.php 1553
+ERROR - 2012-11-16 10:31:02 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at E:\utkorea\base\core\Exceptions.php:185) E:\utkorea\base\libraries\Session.php 672
+ERROR - 2012-11-16 11:01:33 --> Severity: Warning  --> mail() [<a href='function.mail'>function.mail</a>]: SMTP server response: 550 relaying denied E:\utkorea\base\libraries\Email.php 1553
+ERROR - 2012-11-16 11:01:33 --> Severity: Warning  --> Cannot modify header information - headers already sent by (output started at E:\utkorea\base\core\Exceptions.php:185) E:\utkorea\base\libraries\Session.php 672
+ERROR - 2012-11-16 11:12:53 --> Severity: Warning  --> mail() [<a href='function.mail'>function.mail</a>]: SMTP server response: 550 relaying denied E:\utkorea\base\libraries\Email.php 1553
+ERROR - 2012-11-16 11:15:05 --> Severity: Warning  --> mail() [<a href='function.mail'>function.mail</a>]: SMTP server response: 550 relaying denied E:\utkorea\base\libraries\Email.php 1553
+ERROR - 2012-11-16 11:26:47 --> Severity: Warning  --> mail() [<a href='function.mail'>function.mail</a>]: SMTP server response: 550 relaying denied E:\utkorea\base\libraries\Email.php 1553
