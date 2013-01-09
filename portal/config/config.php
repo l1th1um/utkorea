3a11934172d56110f7559e0bd4cd7ae731a5ad40
l1th1um <4r53n1c@gmail.com>
2013-01-10 02:39:08 +0900

    Fix  Login Issue

--- portal/config/config.php
+++ portal/config/config.php
@@ -251,8 +251,8 @@ $config['encryption_key'] = 'q1l49@5*^&';
 |
 */
 $config['sess_cookie_name']		= 'kueh_ut-korea';
-$config['sess_expiration']		= 7200;
-$config['sess_expire_on_close']	= FALSE;
+$config['sess_expiration']		= 86400;
+$config['sess_expire_on_close']	= TRUE;
 $config['sess_encrypt_cookie']	= FALSE;
 $config['sess_use_database']	= true;
 $config['sess_table_name']		= 'utsessions';
